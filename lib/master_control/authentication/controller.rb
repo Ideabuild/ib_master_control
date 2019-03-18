169cfbae7f88324a22f9bb216ccbbedd2ef40b77
Todd Trask <ttrask@gmail.com>
2019-03-18 07:22:46 -0700

    Auth header test

--- lib/master_control/authentication/controller.rb
+++ lib/master_control/authentication/controller.rb
@@ -21,8 +21,8 @@ module MasterControl
 
       def authenticate_user!
         return true if current_user
-        puts "----------------request.headers['Authorization']: ${request.headers['Authorization']}"
-        puts "----------------request.headers['authorization']: ${request.headers['authorization']}"
+        puts "----------------request.headers['Authorization']: #{request.headers['Authorization']}"
+        puts "----------------request.headers['authorization']: #{request.headers['authorization']}"
         
         authenticate_user(request.headers['Authorization']) ||
           raise(MasterControl::Exceptions::AccessDeniedError)
