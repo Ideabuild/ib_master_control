790cabf2ecd1b85b807de1531c2f6acdcbfb74b0
Todd Trask <ttrask@gmail.com>
2023-02-24 10:23:59 -0800

    work group is managed addition

--- lib/master_control/version.rb
+++ lib/master_control/version.rb
@@ -1,4 +1,4 @@
 # frozen_string_literal: true
 module MasterControl
-  VERSION = '1.7.57'
+  VERSION = '1.7.58'
 end
--- spec/models/work_group_spec.rb
+++ spec/models/work_group_spec.rb
@@ -9,11 +9,12 @@ RSpec.describe MasterControl::Models::WorkGroup do
   let(:is_sync_update) { true }
   let(:version) { 1 }
   let(:id) { SecureRandom.uuid }
+  let(:app_model_id) { SecureRandom.uuid }
+  let(:ib_application_id) { SecureRandom.uuid }
+  let(:is_managed) { Faker::Boolean.boolean }
   let(:name) { 'A Name' }
   let(:system_code) { 'the_code' }
   let(:task_app_model_id) { SecureRandom.uuid }
-  let(:app_model_id) { SecureRandom.uuid }
-  let(:ib_application_id) { SecureRandom.uuid }
   let(:created_at) { Time.now.to_s(:iso8601) }
   let(:updated_at) { Time.now.to_s(:iso8601) }
   let(:updated_by_id) { SecureRandom.uuid }
@@ -27,11 +28,12 @@ RSpec.describe MasterControl::Models::WorkGroup do
       is_sync_update: is_sync_update,
       version: version,
       id: id,
+      app_model_id: app_model_id,
+      ib_application_id: ib_application_id,
+      is_managed: is_managed,
       name: name,
       system_code: system_code,
       task_app_model_id: task_app_model_id,
-      app_model_id: app_model_id,
-      ib_application_id: ib_application_id,
       created_at: created_at,
       updated_at: updated_at,
       updated_by_id: updated_by_id,
@@ -54,11 +56,12 @@ RSpec.describe MasterControl::Models::WorkGroup do
       :is_sync_update,
       :id,
       :version,
+      :app_model_id,
+      :ib_application_id,
+      :is_managed,
       :name,
       :system_code,
       :task_app_model_id,
-      :app_model_id,
-      :ib_application_id,
       :created_at,
       :updated_at,
       :updated_by_id,
