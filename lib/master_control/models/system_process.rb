a48b9c8377865be3a4b2626f8e01e3e60d9dceab
Todd Trask <ttrask@gmail.com>
2024-09-18 15:21:26 -0700

    system processes added

---
+++ lib/master_control/models/system_process.rb
@@ -0,0 +1,58 @@
+# frozen_string_literal: true
+module MasterControl
+  module Models
+    class SystemProcess < MasterControl::Models::Base
+      attributes \
+        :id,
+        :ib_application_id,
+        :name,
+        :system_code,
+        :version,
+        :created_at,
+        :updated_at,
+        :updated_by_id,
+        :created_by_id,
+        :active
+
+
+      class << self
+        # rubocop:disable Metrics/MethodLength
+        def json_schema
+          {
+            type: 'object',
+            properties: {
+              id: { type: 'string', pattern: JSON_SCHEMA_PATTERNS[:uuid] },
+              canonical_klass: { type: 'string' },
+              master_control_version: { type: 'string' },
+              version: { type: 'integer' },
+              active: { type: 'boolean' },
+              ib_application_id: { type: 'string', pattern: JSON_SCHEMA_PATTERNS[:uuid] },
+              name: { type: 'string' },
+              system_code: { type: 'string' },
+              created_at: { type: 'string', format: 'date-time' },
+              updated_at: { type: 'string', format: 'date-time' },
+              updated_by_id: { type: 'string', pattern: JSON_SCHEMA_PATTERNS[:uuid] },
+              created_by_id: { type: 'string', pattern: JSON_SCHEMA_PATTERNS[:uuid] }
+            },
+            required: [
+              :canonical_klass,
+              :master_control_version,
+              :version,
+              :id,
+              :active,
+              :ib_application_id,
+              :name,
+              :system_code,
+              :created_at,
+              :updated_at,
+              :updated_by_id,
+              :created_by_id
+            ],
+            additionalProperties: false
+          }.to_json
+        end
+        # rubocop:enable Metrics/MethodLength
+      end
+    end
+  end
+end
---
+++ spec/models/system_process_spec.rb
@@ -0,0 +1,86 @@
+# frozen_string_literal: true
+require 'spec_helper'
+
+RSpec.describe MasterControl::Models::SystemProcess do
+  NULLEABLE_ATTRIBUTES = [].freeze
+
+  let(:canonical_klass) { 'MasterControl::Models::SystemProcess' }
+  let(:master_control_version) { '1.0.0' }
+  let(:version) { 1 }
+  let(:id) { SecureRandom.uuid }
+  let(:ib_application_id) { SecureRandom.uuid }
+  let(:name) { 'Billing' }
+  let(:system_code) { 'billing' }
+  let(:created_at) { Time.now.to_s(:iso8601) }
+  let(:updated_at) { Time.now.to_s(:iso8601) }
+  let(:updated_by_id) { SecureRandom.uuid }
+  let(:created_by_id) { SecureRandom.uuid }
+  let(:active) { true }
+
+  let(:system_process) do
+    {
+      canonical_klass: canonical_klass,
+      master_control_version: master_control_version,
+      version: version,
+      id: id,
+      ib_application_id: ib_application_id,
+      name: name,
+      system_code: system_code,
+      created_at: created_at,
+      updated_at: updated_at,
+      updated_by_id: updated_by_id,
+      created_by_id: created_by_id,
+      active: active
+    }
+  end
+
+  def validate!
+    JSON::Validator.validate!(MasterControl::Models::SystemProcess.json_schema, system_process)
+  end
+
+  context 'happy path' do
+    specify { expect { validate! }.to_not raise_error }
+  end
+
+  describe 'required attributes (not nil)' do
+    [
+      :canonical_klass,
+      :id,
+      :master_control_version,
+      :version,
+      :ib_application_id,
+      :name,
+      :system_code,
+      :created_at,
+      :updated_at,
+      :updated_by_id,
+      :created_by_id
+    ].each do |attribute|
+      context attribute do
+        let(attribute) { nil }
+
+        specify { expect { validate! }.to raise_error JSON::Schema::ValidationError }
+      end
+    end
+  end
+
+  context 'nullable attributes' do
+    NULLEABLE_ATTRIBUTES.each do |attribute|
+      context attribute do
+        let(attribute) { nil }
+
+        specify { expect { validate! }.to_not raise_error }
+      end
+    end
+  end
+
+  context 'format validations' do
+    [:created_at, :updated_at].each do |attribute|
+      context attribute do
+        let(attribute) { '' }
+
+        specify { expect { validate! }.to raise_error JSON::Schema::ValidationError }
+      end
+    end
+  end
+end
